c02a54a4543b720b1792e5ac7ee320efb3a8a7d2
Soumyadip Bhowmik <105037882+SoumyadipBhowmik@users.noreply.github.com>
2022-11-09 23:12:12 +0530

    Delete background.py

--- background.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-def draw_background(screen, background, screen_width, screen_height):
-    bg_scaled = pygame.transform.scale(background, (screen_width, screen_height))
-    screen.blit(bg_scaled, (0,0))

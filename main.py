6bc126b222ec157a2915ae6b51c212118c65fc0c
SoumyadipBhowmik <bhowmik.soumyadip9@gmail.com>
2022-11-12 18:49:43 +0530

    lots of changes. missed streak due to college

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
 from player import Fighter
-from functions import draw_health_bar
-from functions import draw_background
+from functions import *
 
+#screen size
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+
+#warrior and wizard sizes
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 3.6
+WARRIOR_OFFSET = [72, 60]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 117]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
 FPS = 60
 
@@ -12,25 +22,25 @@ FPS = 60
 pygame.init()
 pygame.display.set_caption("Fighter")
 
+#step definition
+WARRIOR_ANIMATION_LIST = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_LIST = [8, 8, 1, 8, 8, 3, 7]
+
 #SCREEN_DISPLAY
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 run = True
 
+#load spritesheets
+warrior_sheet = pygame.image.load("graphics\warrior_sprites\warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("graphics\wizard_sprites\wizard.png").convert_alpha()
+
 #FIGHTERS
-player1 = Fighter(200,420, WARRIOR_ANIMATION_LIST)
-player2 = Fighter(700, 420, WIZARD_ANIMATION_LIST)
+player1 = Fighter(200, 420, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_LIST)
+player2 = Fighter(700, 420, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_LIST)
 
 
 background = pygame.image.load("graphics/bg.jpg").convert_alpha()
-#load spritesheets
-warrior_sheet = pygame.load.image("graphics\warrior_sprites\warrior.png").convert_alpha()
-wizard_sheet = pygame.load.image("graphics\wizard_sprites\wizard.png").convert_alpha()
-
-
-#step definition
-WARRIOR_ANIMATION_LIST = [10, 8, 1, 7, 7, 3, 7]
-WIZARD_ANIMATION_LIST = [8, 8, 1, 8, 8, 3, 7]
 
 
 while run:
@@ -41,14 +51,20 @@ while run:
     draw_health_bar(player1.health, screen, 20, 40)
     draw_health_bar(player2.health, screen, 580, 40)
     #DISPLAY Fighters
+    player1.update()
     player1.draw(screen)
     player1.move(SCREEN_WIDTH, SCREEN_HEIGHT, pygame.K_a, pygame.K_d, pygame.K_w, screen, player2)
+    player2.update()
     player2.draw(screen)
     player2.move(SCREEN_WIDTH, SCREEN_HEIGHT, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, screen, player1)
 
+    qkey = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if qkey[pygame.K_LCTRL]:
+            if qkey[pygame.K_w]:
+                run = False
     pygame.display.update()
     clock.tick(FPS)
 
--- player.py
+++ player.py
@@ -1,14 +1,34 @@
 import pygame
 
 class Fighter:
-    def __init__(self, x, y):
+    def __init__(self, x, y, data, spritesheets, animation_steps):
+        self.size = data[0]
+        self.imageSCALED = data[1]
+        self.offset = data[2]
         self.flip = False
+        self.animationList = self.load_images(spritesheets, animation_steps)
+        self.action = 0 #if 0 = idle, 1 = run, 2 = jump, 3 = attack 1, 4 = attack 2, 5 = hit, 6 = death
+        self.frameINDEX = 0
+        self.image = self.animationList[self.action][self.frameINDEX]
+        self.animation_timer = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 150)) #x and y are used to place the characters in ground
         self.velocity_y = 0
         self.jump = False
         self.attacking = False
         self.attack_type = 0
         self.health = 100
+
+    def load_images(self, spritesheets, animation_steps):
+        SPRITEBOX = self.size * self.imageSCALED
+        #this function is used to extract images from the spritesheet
+        animationList = []
+        for y, animation in enumerate(animation_steps):
+            tempImgLIST = []
+            for x in range(animation):
+                tempImg = spritesheets.subsurface(x * self.size, y * self.size, self.size, self.size)
+                tempImgLIST.append(pygame.transform.scale(tempImg, (SPRITEBOX, SPRITEBOX)))
+            animationList.append(tempImgLIST)
+        return animationList
 
     def move(self, screen_width, screen_height, left_key, right_key, jump, screen, target):
         #VARIABLES FOR MOVEMENT
@@ -62,6 +82,18 @@ class Fighter:
         self.rect.x += dx
         self.rect.y += dy
 
+    #HANDLE ANIMATION UPDATES
+    def update(self):
+        animation_cooldown = 100
+        self.image = self.animationList[self.action][self.frameINDEX]
+        #check if the time in the current frame has passed some miliseconds
+        if pygame.time.get_ticks() - self.animation_timer > animation_cooldown:
+            self.frameINDEX += 1
+            self.animation_timer = pygame.time.get_ticks()
+        #check if the animation has finished
+        if self.frameINDEX >= len(self.animationList[self.action]):
+            self.frameINDEX = 0
+
     def attack(self, screen, target):
         attack_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
 
@@ -72,4 +104,6 @@ class Fighter:
         pygame.draw.rect(screen, (255, 255, 100), attack_rect)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 100, 0), self.rect)
+        img = pygame.transform.flip(self.image, self.flip, False)
+        #pygame.draw.rect(screen, (255, 100, 0), self.rect)
+        screen.blit(img, (self.rect.x - (self.offset[0] * self.imageSCALED), self.rect.y - (self.offset[1] * self.imageSCALED)))
